6113a70bc619fe52f3696d9186babc2c80f76773
lucky-nkwor <uwaenyase@gmail.com>
2022-09-15 15:11:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ *  *  Description: main - function main begins the execution of the program
+ *
+ *   *  Return: 0 indicates that the programme is successful
+ *
+ *    */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
